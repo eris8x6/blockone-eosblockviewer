71f824df4491d4e67435739d1eb169d09f3d7f1c
J. Christopher Phelan <chris@chrisphelan.me>
2019-01-04 14:06:41 -0500

    Quick code cleanup and removal of YAGNI from templates etc

--- src/main/java/me/chrisphelan/blockone/eosblockviewer/BlockListFragment.kt
+++ src/main/java/me/chrisphelan/blockone/eosblockviewer/BlockListFragment.kt
@@ -1,16 +1,13 @@
 package me.chrisphelan.blockone.eosblockviewer
 
-import android.opengl.Visibility
 import android.support.v4.app.Fragment
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.fragment_block_list.*
-import kotlinx.coroutines.*
 
 class BlockListFragment : Fragment() {
 
@@ -33,12 +30,5 @@ class BlockListFragment : Fragment() {
         layoutManager = LinearLayoutManager( this@BlockListFragment.context)
         setHasFixedSize(true)
     }
-
-    private fun dummyData(): List<BlockData> = listOf(
-        BlockData("today", "Me", "2", listOf( TransactionData( "foo" )) ),
-        BlockData("yesterday", "Someone Else", "1", listOf( TransactionData( "bar" )) ),
-        BlockData("beginning of time", "God", "root", listOf( TransactionData( "bletch" )) )
-    )
-
 
 }
--- src/main/java/me/chrisphelan/blockone/eosblockviewer/BlockViewerActivity.kt
+++ src/main/java/me/chrisphelan/blockone/eosblockviewer/BlockViewerActivity.kt
@@ -3,29 +3,17 @@ package me.chrisphelan.blockone.eosblockviewer
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_block_viewer.*
 
 val LOG_TAG = "eosblockviewer"
 
 class BlockViewerActivity : AppCompatActivity() {
 
-    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_home -> {
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_dashboard -> {
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-        false
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_block_viewer)
 
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        // TODO Adjust this for screen rotation
+        // TODO Adjust this for change of views
 
         supportFragmentManager.let{
             if (it.findFragmentById(R.id.fragment_container) == null) {
